6a763f1f4a5cfe005bea31ff6a0cd9ea623b990c
TexLuciano <lucianomartinello15@gmail.com>
2023-03-06 21:21:26 -0300

    test build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import useFetch from './CustomHooks/useFech';
 import Pagination from './components/Pagination';
 import qs from 'qs';
@@ -8,17 +7,6 @@ import Catogorias from './components/Catogorias';
 import NavBar from './components/Navbar';
 import { FavoritosList } from './components/Favoritos';
 import Data from './components/Data';
-=======
-import Search from './components/Search';
-import useFetch from './CustomHooks/useFech';
-import { Nav, Favoritos, Adicionado } from './style/style';
-import logo from './img/icone.PNG';
-import Pagination from './components/Pagination';
-import qs from 'qs';
-import hambuguer from './img/hamburger.png';
-import Catogorias from './components/Catogorias';
-import x from './img/x.png';
->>>>>>> 3ffbb6d9a7d794d4a3854b9379901fdd0f6809a1
 
 const getDatafromLS = () => {
   const data = localStorage.getItem('anime');
@@ -39,11 +27,7 @@ function App() {
   const [adicionado, setAdicionado] = useState(false);
   const [mobile, setMobile] = useState(false);
 
-<<<<<<< HEAD
   const LIMIT = 14;
-=======
-  const LIMIT = 12;
->>>>>>> 3ffbb6d9a7d794d4a3854b9379901fdd0f6809a1
   const query = {
     page: {
       limit: LIMIT,
@@ -51,18 +35,9 @@ function App() {
     },
   };
 
-<<<<<<< HEAD
  
 
   console.log(data);
-=======
-  const apagarAnime = (index) => {
-    const att = favorito.filter((item) => index !== item.id);
-    setFavorito(att);
-  };
-
-console.log(data)
->>>>>>> 3ffbb6d9a7d794d4a3854b9379901fdd0f6809a1
   useEffect(() => {
     if (text) {
       query.filter = {
@@ -100,37 +75,17 @@ console.log(data)
     setAdicionado(true);
   }
 
-<<<<<<< HEAD
-=======
-  function retorna() {
-    if (offset !== 0) {
-      setOffset((offset) => (offset = 0));
-    }
-  }
-
->>>>>>> 3ffbb6d9a7d794d4a3854b9379901fdd0f6809a1
   useEffect(() => {
     if (favorito) localStorage.setItem('anime', JSON.stringify(favorito));
   }, [favorito]);
 
   return (
     <div>
-<<<<<<< HEAD
       <NavBar setText={setText} text={text} />
-=======
-      <Nav>
-        <img src={logo} alt="logo" className="clicavel" onClick={retorna} />
-        <Search value={text} onChange={(str) => setText(str)} />
-        <button className="close-btn" onClick={() => setMobile(!mobile)}>
-          <img className="close-img" src={mobile ? x : hambuguer} alt="" />
-        </button>
-      </Nav>
->>>>>>> 3ffbb6d9a7d794d4a3854b9379901fdd0f6809a1
       <div className={mobile ? 'menu-mobile' : 'btns'}>
         <button className="favoritos-btn " onClick={modal}>
           Favoritos
         </button>
-<<<<<<< HEAD
         <Catogorias setCategoria={setCategoria} />
       </div>
       <FavoritosList
@@ -142,67 +97,6 @@ console.log(data)
         setAdicionado={setAdicionado}
       />
       <Data data={data} loading={loading} error={error} addFavorito={addFavorito}/>
-=======
-
-        <Catogorias setCategoria={setCategoria} />
-      </div>
-      {adicionado && <Adicionado>Favorito Adicionado</Adicionado>}
-      {modalFavorito ? (
-        <Favoritos ativo={modalFavorito}>
-          <li>
-            <input type="button" value="x" onClick={modal} />
-          </li>
-          {favorito && favorito.length ? (
-            favorito.map((item) => (
-              <li key={item.id}>
-                <p>{item.name}</p>
-                <button onClick={() => apagarAnime(item.id)}>x</button>
-              </li>
-            ))
-          ) : (
-            <p>Você não tem Favoritos </p>
-          )}
-        </Favoritos>
-      ) : null}
-
-      {data && data.data.length < 2 && <p>não encontrado</p>}
-      {error && <p>{error}</p>}
-      {loading && <div className="carregando"></div>}
-      {data && (
-        <ul className="container-grid">
-          {data.data.map((item) => (
-            <li key={item.id}>
-              <h5>{item.attributes.canonicalTitle}</h5>
-              <div className="anime-info">
-                <div className="overlay">
-                  <p className="synopsis">
-                    ➤Inicio:{item.attributes.startDate}
-                    <br />
-                    ➤Fim:
-                    {item.attributes.endDate ? item.attributes.endDate : '?'}
-                    <br />
-                    <br />➤{item.attributes.description}
-                  </p>
-                </div>
-                <img
-                  src={item.attributes.posterImage.small}
-                  alt={item.attributes.canonicalTitle}
-                />
-              </div>
-
-              <button
-                className="add-favorito"
-                value={item.attributes.canonicalTitle}
-                onClick={addFavorito}
-              >
-                +Favorito
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-
->>>>>>> 3ffbb6d9a7d794d4a3854b9379901fdd0f6809a1
       {}
       {data && data.meta ? (
         <Pagination
